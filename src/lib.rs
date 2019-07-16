b8bcce1b90dc93e98a4432292b3d0eb1336e3fda
Bart Massey <bart@cs.pdx.edu>
2019-07-16 16:38:04 -0700

    applied license to files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright © 2019 Bart Massey
+// [This program is licensed under the "MIT License"]
+// Please see the file LICENSE in the source
+// distribution of this software for license terms.
 
 //! Lazy grouping by key across boundaries.
 
